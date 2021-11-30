38a4a97f35d3c993fccd5d84115e84a5a3b73625
LashaKakabadze <l.qqbadze@gmail.com>
2021-11-30 18:50:19 +0400

    color variables :sunglasses:

---
+++ src/helpers/colorPalette.ts
@@ -0,0 +1,9 @@
+export const COLOR_BLACK = "#000";
+export const COLOR_WHITE = "#fff";
+export const COLOR_GRAY = "#787f85";
+export const COLOR_DARK_GRAY = "#999";
+export const COLOR_MEDIUM_GRAY = "#333333";
+export const COLOR_LIGHT_GRAY = "#e5e7eb";
+export const COLOR_BLUE = "#0b63f3";
+export const COLOR_DARK_BLUE = "rgb(24, 33, 109)";
+export const COLOR_DARK_ORANGE = "rgb(255, 130, 92)";
